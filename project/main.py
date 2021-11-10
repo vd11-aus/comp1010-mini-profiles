76ecf9b1bc232d06751373029953ffb49e190932
vd11-unsw <v.dinh@student.unsw.edu.au>
2021-11-11 04:13:18 +1100

    v1.1.11

--- project/main.py
+++ project/main.py
@@ -1,3 +1,4 @@
+from os import write
 from flask import *
 import os.path
 import firebase_admin
@@ -16,34 +17,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "25ca747914f8d844c9256dfba73c39fe1ae62290da552a7f14d83e194aa98edd"
 
 def write_json(data, filename):
-    f = open(f'JSON/{filename}.json', 'w')
+    f = open(f'project/JSON/{filename}.json', 'w')
     f.write(json.dumps(data))
     f.close()
 
 def read_json(filename):
-    f = open(f'JSON/{filename}.json', 'r')
+    f = open(f'project/JSON/{filename}.json', 'r')
     data = json.loads(f.read())
     f.close()
     return data
 
 def create_user(zid, password):
     credentials_data = {
-        "zid": zid,
+        "zId": zid,
         "password": password
     }
     profile_data = {
         "name": "",
         "gender": "",
-        "aboutme": "",
+        "aboutMe": "",
     }
     fs_link.collection("credentials").document(zid).set(credentials_data)
     fs_link.collection("profiles").document(zid).set(profile_data)
-
-jsonlist = ["error-list"]
-
-for filename in jsonlist:
-    if os.path.exists(f"JSON/{filename}.json") == False:
-        write_json({}, filename)
 
 @app.route("/", methods=["GET", "POST"])
 def signin():
@@ -53,10 +48,10 @@ def signin():
             if credentials_grab.exists:
                 credentials_dict = credentials_grab.to_dict()
                 if credentials_dict["password"] != request.form["password"]:
-                    return redirect(url_for("error", reasoncode = "100"))
+                    return redirect(url_for("error", reasoncode = "100", previouspage = "signin"))
                 return redirect(url_for("home"))
             else:
-                return redirect(url_for("error", reasoncode = "101"))
+                return redirect(url_for("error", reasoncode = "101", previouspage = "signin"))
         if "createaccount" in request.form:
             return redirect(url_for("createaccount"))
     return render_template("signin.html")
@@ -69,13 +64,13 @@ def createaccount():
         if "confirmcreateaccount" in request.form:
             current_user_grab = fs_link.collection("credentials").document(request.form["zid"]).get()
             if current_user_grab.exists:
-                return redirect(url_for("error", reasoncode = "200"))
+                return redirect(url_for("error", reasoncode = "200", previouspage = "createaccount"))
             number_count = 0
             for character in request.form["zid"]:
                 if character in string.digits:
                     number_count += 1
             if len(request.form["zid"]) != 8 or request.form["zid"][0] != "z" or number_count != 7:
-                return redirect(url_for("error", reasoncode = "201"))
+                return redirect(url_for("error", reasoncode = "201", previouspage = "createaccount"))
             password_valid = True
             number_lower_case = 0
             number_upper_case = 0
@@ -87,24 +82,36 @@ def createaccount():
                     number_digits += 1
                 elif character in string.ascii_lowercase:
                     number_lower_case += 1
-                elif character in string.string.ascii_uppercase:
+                elif character in string.ascii_uppercase:
                     number_upper_case += 1
             if number_lower_case == 0 or number_upper_case == 0 or number_digits == 0:
                 password_valid = False
             if password_valid == False:
-                return redirect(url_for("error", reasoncode = "202"))
+                return redirect(url_for("error", reasoncode = "202", previouspage = "createaccount"))
             if request.form["password"] != request.form["confirmpassword"]:
-                return redirect(url_for("error", reasoncode = "203"))
-            return redirect(url_for("home"))
+                return redirect(url_for("error", reasoncode = "203", previouspage = "createaccount"))
+            create_user(request.form["zid"], request.form["password"])
+            return redirect(url_for("signin"))
     return render_template("createaccount.html")
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
     return "Welcome back!"
 
-@app.route("/error/<reasoncode>", methods=["GET", "POST"])
-def error(reasoncode):
-    return reasoncode
+@app.route("/error/<reasoncode>-from-<previouspage>", methods=["GET", "POST"])
+def error(reasoncode, previouspage):
+    if request.method == "POST":
+        if "goback" in request.form:
+            return redirect(url_for(previouspage))
+    error_directory_grab = fs_link.collection("other").document("errordirectory").get()
+    if error_directory_grab.exists:
+        dataset = error_directory_grab.to_dict()
+        dynamic_data = dataset["errordirectory"]
+    write_json(dynamic_data, "error-list")
+    error_directory = read_json("error-list")
+    title = error_directory[reasoncode]["title"]
+    reason = error_directory[reasoncode]["reason"]
+    return render_template("error.html", errortitle = title, errornumber = reasoncode, errormessage = reason)
 
 if __name__ == "__main__":
     app.run(debug=True)
