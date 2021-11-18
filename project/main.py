129df01d61ac53237eb102c5b79fe2ed8edbf0f3
vd11-unsw <v.dinh@student.unsw.edu.au>
2021-11-18 18:50:34 +1100

    v1.2.5

--- project/main.py
+++ project/main.py
@@ -38,6 +38,24 @@ def read_json(filename):
     f.close()
     return data
 
+def check_password_strength(password):
+    password_valid = True
+    number_lower_case = 0
+    number_upper_case = 0
+    number_digits = 0
+    if len(password) < 8:
+        password_valid = False
+    for character in password:
+        if character in string.digits:
+            number_digits += 1
+        elif character in string.ascii_lowercase:
+            number_lower_case += 1
+        elif character in string.ascii_uppercase:
+            number_upper_case += 1
+    if number_lower_case == 0 or number_upper_case == 0 or number_digits == 0:
+        password_valid = False
+    return password_valid
+
 def create_user(zid, password):
     credentials_data = {
         "zId": zid,
@@ -55,6 +73,11 @@ def create_user(zid, password):
     }
     fs_link.collection("credentials").document(zid).set(credentials_data)
     fs_link.collection("profiles").document(zid).set(profile_data)
+    userset_data_grab = fs_link.collection("profiles").document("userset").get()
+    if userset_data_grab.exists:
+        userset = userset_data_grab.to_dict()
+    userset[zid] = "Change Name"
+    fs_link.collection("profiles").document("userset").set(userset)
 
 def update_icon():
     userid = session["currentuser"]
@@ -72,6 +95,11 @@ def save_profile(name, gender, aboutme, interests, facebook, twitter, instagram)
         "instagram": instagram
     }
     fs_link.collection("profiles").document(session["currentuser"]).set(profile_data)
+    userset_data_grab = fs_link.collection("profiles").document("userset").get()
+    if userset_data_grab.exists:
+        userset = userset_data_grab.to_dict()
+    userset[session["currentuser"]] = name
+    fs_link.collection("profiles").document("userset").set(userset)
 
 def save_courses(selectedcourses):
     course_data_grab = fs_link.collection("other").document("courses").get()
@@ -89,6 +117,13 @@ def save_courses(selectedcourses):
         if course_data["courses"][course]["students"].count(session["currentuser"]) > 0:
             course_data["courses"][course]["students"].remove(session["currentuser"])
     fs_link.collection("other").document("courses").set(course_data)
+
+def save_password(password):
+    credentials_data_grab = fs_link.collection("credentials").document(session["currentuser"]).get()
+    if credentials_data_grab.exists:
+        credentials_data = credentials_data_grab.to_dict()
+    credentials_data["password"] = password
+    fs_link.collection("credentials").document(session["currentuser"]).set(credentials_data)
 
 def save_strengths(selectedstrengths):
     strength_data_grab = fs_link.collection("other").document("strengths").get()
@@ -122,7 +157,18 @@ def filter_students_courses(filters):
                 break
     for removed in students_to_remove:
         filtered_students.remove(removed)
-    session["filterstudentlist"] = filtered_students
+    userset_data_grab = fs_link.collection("profiles").document("userset").get()
+    if userset_data_grab.exists:
+        userset = userset_data_grab.to_dict()
+    outcome = {}
+    for student in filtered_students:
+        zidvalue = str(student)
+        namevalue = userset[zidvalue]
+        iconvalue = st_link.blob(f"icons/{zidvalue}.png").generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+        outcome[student] = {}
+        outcome[student]["name"] = namevalue
+        outcome[student]["icon"] = iconvalue
+    session["filterstudentlist"] = outcome
 
 def filter_students_strengths(filters):
     strength_data_grab = fs_link.collection("other").document("strengths").get()
@@ -139,7 +185,18 @@ def filter_students_strengths(filters):
                 break
     for removed in students_to_remove:
         filtered_students.remove(removed)
-    session["filterstudentlist"] = filtered_students
+    userset_data_grab = fs_link.collection("profiles").document("userset").get()
+    if userset_data_grab.exists:
+        userset = userset_data_grab.to_dict()
+    outcome = {}
+    for student in filtered_students:
+        zidvalue = str(student)
+        namevalue = userset[zidvalue]
+        iconvalue = st_link.blob(f"icons/{zidvalue}.png").generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+        outcome[student] = {}
+        outcome[student]["name"] = namevalue
+        outcome[student]["icon"] = iconvalue
+    session["filterstudentlist"] = outcome
 
 # COMPLETE - SIGN IN
 @app.route("/", methods=["GET", "POST"])
@@ -177,22 +234,7 @@ def createaccount():
                     number_count += 1
             if len(request.form["zid"]) != 8 or request.form["zid"][0] != "z" or number_count != 7:
                 return redirect(url_for("error", reasoncode = "201", previouspage = "createaccount"))
-            password_valid = True
-            number_lower_case = 0
-            number_upper_case = 0
-            number_digits = 0
-            if len(request.form["password"]) < 8:
-                password_valid = False
-            for character in request.form["password"]:
-                if character in string.digits:
-                    number_digits += 1
-                elif character in string.ascii_lowercase:
-                    number_lower_case += 1
-                elif character in string.ascii_uppercase:
-                    number_upper_case += 1
-            if number_lower_case == 0 or number_upper_case == 0 or number_digits == 0:
-                password_valid = False
-            if password_valid == False:
+            if check_password_strength(request.form["password"]) == False:
                 return redirect(url_for("error", reasoncode = "202", previouspage = "createaccount"))
             if request.form["password"] != request.form["confirmpassword"]:
                 return redirect(url_for("error", reasoncode = "203", previouspage = "createaccount"))
@@ -210,11 +252,6 @@ def introduction():
     profile_data_grab = fs_link.collection("profiles").document(session["currentuser"]).get()
     if profile_data_grab.exists:
         profile_data = profile_data_grab.to_dict()
-    newsfeed_data_grab = fs_link.collection("other").document("newsfeed").get()
-    if newsfeed_data_grab.exists:
-        newsfeed_data = newsfeed_data_grab.to_dict()["articlelist"]
-        flipped_data = newsfeed_data[::-1]
-        newest_to_oldest = flipped_data[0:11]
     if request.method == "POST":
         if "logout" in request.form:
             session["currentuser"] = ""
@@ -227,8 +264,7 @@ def introduction():
             return redirect(url_for("search"))
         if "help" in request.form:
             return redirect(url_for("help"))
-    return render_template("introduction.html", name = profile_data["name"], zid = session["currentuser"], newsfeed = newest_to_oldest, 
-        backgroundimage = background_generator())
+    return render_template("introduction.html", name = profile_data["name"], zid = session["currentuser"], backgroundimage = background_generator())
 
 # COMPLETE - HOME
 @app.route("/home", methods=["GET", "POST"])
@@ -259,7 +295,7 @@ def home():
     return render_template("home.html", name = profile_data["name"], zid = session["currentuser"], newsfeed = newest_to_oldest, 
         backgroundimage = background_generator())
 
-# COMPLETE - SETTINGS
+# INCOMPLETE - SETTINGS
 @app.route("/settings", methods=["GET", "POST"])
 def settings():
     if session["currentuser"] == "":
@@ -312,12 +348,56 @@ def settings():
         if "savestrengths" in request.form:
             save_strengths(request.form.getlist("strengthselection"))
             return redirect(url_for("home"))
+        if "viewprofile" in request.form:
+            return redirect(url_for("profile", inputtedid = session["currentuser"]))
+        if "changepassword" in request.form:
+            return redirect(url_for("changepassword"))
     return render_template("settings.html", name = profile_data["name"], zid = userid, aboutme = profile_data["aboutMe"], 
         gender = profile_data["gender"], genderlist = all_genders, facebook = profile_data["facebook"], interests = profile_data["interests"], 
         twitter = profile_data["twitter"], instagram = profile_data["instagram"], courselist = sorted(course_data), enrolled = enrolledcourses,
         strengthlist = sorted(strength_data), userstrengths = acknowledgedstrengths, backgroundimage = background_generator())
 
-# INCOMPLETE - SEARCH
+# INCOMPLETE - CHANGE PASSWORD
+@app.route("/changepassword", methods=["GET", "POST"])
+def changepassword():
+    if session["currentuser"] == "":
+        return redirect(url_for("error", reasoncode = "900", previouspage = "signin"))
+    update_icon()
+    profile_data_grab = fs_link.collection("profiles").document(session["currentuser"]).get()
+    if profile_data_grab.exists:
+        profile_data = profile_data_grab.to_dict()
+    if request.method == "POST":
+        if "logout" in request.form:
+            session["currentuser"] = ""
+            return redirect(url_for("signin"))
+        if "settings" in request.form:
+            return redirect(url_for("settings"))
+        if "home" in request.form:
+            return redirect(url_for("home"))
+        if "search" in request.form:
+            return redirect(url_for("search"))
+        if "help" in request.form:
+            return redirect(url_for("help"))
+        if "cancel" in request.form:
+            return redirect(url_for("settings"))
+        if "changepassword" in request.form:
+            originalpassword = request.form["oldpassword"]
+            newpassword = request.form["newpassword"]
+            confirmedpassword = request.form["confirmpassword"]
+            credentials_grab = fs_link.collection("credentials").document(session["currentuser"]).get()
+            if credentials_grab.exists:
+                credentials_dict = credentials_grab.to_dict()
+            if credentials_dict["password"] != originalpassword:
+                return redirect(url_for("error", reasoncode = "100", previouspage = "changepassword"))
+            if check_password_strength(newpassword) == False:
+                return redirect(url_for("error", reasoncode = "202", previouspage = "changepassword"))
+            if confirmedpassword != newpassword:
+                return redirect(url_for("error", reasoncode = "203", previouspage = "changepassword"))
+            save_password(newpassword)
+            return redirect(url_for("settings"))
+    return render_template("changepassword.html", name = profile_data["name"], zid = session["currentuser"],  backgroundimage = background_generator())
+
+# COMPLETE - SEARCH
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if session["currentuser"] == "":
@@ -345,21 +425,25 @@ def search():
         if "help" in request.form:
             return redirect(url_for("help"))
         if "searchbyid" in request.form:
-            return redirect(url_for("student", inputtedid = request.form["zidinput"]))
+            return redirect(url_for("profile", inputtedid = request.form["zidinput"]))
         if "searchbycourse" in request.form:
             if len(request.form.getlist("courseselection")) > 0:
                 filter_students_courses(request.form.getlist("courseselection"))
-                return redirect(url_for("results", type = "course", criteria = request.form.getlist("courseselection")))
+                session["filtertype"] = "course"
+                session["filtercriteria"] = request.form.getlist("courseselection")
+                return redirect(url_for("results"))
         if "searchbystrength" in request.form:
             if len(request.form.getlist("strengthselection")) > 0:
                 filter_students_strengths(request.form.getlist("strengthselection"))
-                return redirect(url_for("results", type = "strength", criteria = request.form.getlist("strengthselection")))
+                session["filtertype"] = "strength"
+                session["filtercriteria"] = request.form.getlist("strengthselection")
+                return redirect(url_for("results"))
     return render_template("search.html", name = profile_data["name"], zid = session["currentuser"], courselist = sorted(course_data), 
         strengthlist = sorted(strength_data), backgroundimage = background_generator())
 
-# INCOMPLETE - RESULTS
-@app.route("/search/<type>-<criteria>", methods=["GET", "POST"])
-def results(type, criteria):
+# COMPLETE - RESULTS
+@app.route("/search/results", methods=["GET", "POST"])
+def results():
     if session["currentuser"] == "":
         return redirect(url_for("error", reasoncode = "900", previouspage = "signin"))
     update_icon()
@@ -378,11 +462,13 @@ def results(type, criteria):
             return redirect(url_for("search"))
         if "help" in request.form:
             return redirect(url_for("help"))
-    return render_template("results.html", name = profile_data["name"], zid = session["currentuser"], filtertype = type, resultcriteria = criteria, studentlist = session["filterstudentlist"], backgroundimage = background_generator())
+        if "viewprofile" in request.form:
+            return redirect(url_for("profile", inputtedid = request.form["studentredirect"]))
+    return render_template("results.html", name = profile_data["name"], zid = session["currentuser"], filtertype = session["filtertype"], resultcriteria = session["filtercriteria"], studentlist = session["filterstudentlist"], backgroundimage = background_generator())
 
-# INCOMPLETE - STUDENT
-@app.route("/student/<inputtedid>", methods=["GET", "POST"])
-def student(inputtedid):
+# INCOMPLETE - PROFILE
+@app.route("/profile/<inputtedid>", methods=["GET", "POST"])
+def profile(inputtedid):
     if session["currentuser"] == "":
         return redirect(url_for("error", reasoncode = "900", previouspage = "signin"))
     update_icon()
@@ -392,6 +478,21 @@ def student(inputtedid):
     student_data_grab = fs_link.collection("profiles").document(inputtedid).get()
     if student_data_grab.exists:
         student_data = student_data_grab.to_dict()
+    student_data["icon"] = st_link.blob(f"icons/{inputtedid}.png").generate_signed_url(datetime.timedelta(seconds=300), method='GET')
+    course_data_grab = fs_link.collection("other").document("courses").get()
+    if course_data_grab.exists:
+        course_data = course_data_grab.to_dict()["courses"]
+    strength_data_grab = fs_link.collection("other").document("strengths").get()
+    if strength_data_grab.exists:
+        strength_data = strength_data_grab.to_dict()["strengths"]
+    acknowledgedstrengths = []
+    for strength in strength_data:
+        if strength_data_grab.to_dict()["strengths"][strength]["students"].count(inputtedid) > 0:
+            acknowledgedstrengths.append(str(strength))
+    enrolledcourses = []
+    for course in course_data:
+        if course_data_grab.to_dict()["courses"][course]["students"].count(inputtedid) > 0:
+            enrolledcourses.append(str(course))
     if request.method == "POST":
         if "logout" in request.form:
             session["currentuser"] = ""
@@ -404,7 +505,8 @@ def student(inputtedid):
             return redirect(url_for("search"))
         if "help" in request.form:
             return redirect(url_for("help"))
-    return render_template("student.html", name = profile_data["name"], zid = session["currentuser"], studentid = inputtedid, backgroundimage = background_generator())
+    return render_template("profile.html", name = profile_data["name"], zid = session["currentuser"], studentid = inputtedid, 
+        studentprofile = student_data, strengthlist = acknowledgedstrengths, courselist = enrolledcourses, backgroundimage = background_generator())
 
 # COMPLETE - ERROR PAGE
 @app.route("/error/<reasoncode>-from-<previouspage>", methods=["GET", "POST"])
